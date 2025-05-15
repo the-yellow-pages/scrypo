1b23e6aef90cb0c4ea187246de7a7852df74a4a6
NEVEROV iaroslav <beeguy74@gmail.com>
2025-05-15 18:55:48 +0200

    first interaction with blockchain

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,8 +1,18 @@
+import { SendERC20 } from 'components/Transactions/ERC20';
+import { useState } from 'react';
+
 function Profile() {
+    const [lastTxError, setLastTxError] = useState("");
     return (
         <div>
             <h2>Profile Page</h2>
             <p>This is the Profile page content.</p>
+            <SendERC20 setLastTxError={setLastTxError} />
+            {lastTxError && (
+                <div className="error-message">
+                    <p>Error: {lastTxError}</p>
+                </div>
+            )}
         </div>
     );
 }
